150321755f7f1ce4e5597006fa4ad378dff03e16
Jose Fresco <josemariafresco@gmail.com>
2015-08-05 17:07:20 -0300

    Added predicate model

---
+++ src/data/models/predicate.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+var Predicate = new Schema({
+  statement: { type: String, required: true },
+  fulfillment: { type: Number, default: 0, min: 0, max: 100 },
+  lastUpdate: { type: Date, default: Date.now }
+})
+
+export default mongoose.model('Predicate', Predicate)
--- src/data/models/subject.js
+++ src/data/models/subject.js
@@ -1,8 +1,15 @@
 import mongoose from 'mongoose'
+import Predicate from './predicate'
+
 const Schema = mongoose.Schema
 
 var Subject = new Schema({
-  name: { type: String }
+  identifier: { type: String, required: true },
+  name: { type: String, required: true },
+  description: { type: String },
+  begin: { type: Date, default: Date.now, required: true },
+  end: { type: Date, required: true },
+  promises: [Predicate]
 })
 
 export default mongoose.model('Subject', Subject)
