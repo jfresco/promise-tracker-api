3a00765f61280b2bdb6b1c25c7a03e5e7720ce17
Jose Fresco <josemariafresco@gmail.com>
2015-08-12 13:46:41 -0300

    Get MongoDB URI from env variable MONGOLAB_URL

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,6 @@ const env = process.env
 export default {
   appName: env.APP_NAME || 'Promise Tracker API',
   port: env.PORT || 8000,
-  dbURI: env.DB_URI || env.MONGO_URL || env.MONGOHQ_URL || 'mongodb://localhost/promisetracker',
+  dbURI: env.DB_URI || env.MONGO_URL || env.MONGOLAB_URI || 'mongodb://localhost/promisetracker',
   secret: env.SECRET || 'change this string'
 }
