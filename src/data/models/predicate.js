9156bb38902f0310639d62135a5654dcc2945624
Jose Fresco <josemariafresco@gmail.com>
2015-08-12 10:44:50 -0300

    Predicate: added impossible field

--- src/data/models/predicate.js
+++ src/data/models/predicate.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema
 var Predicate = new Schema({
   statement: { type: String, required: true },
   fulfillment: { type: Number, default: 0, min: 0, max: 100 },
+  impossible: { type: Boolean, default: false },
   lastUpdate: { type: Date, default: Date.now }
 })
 
